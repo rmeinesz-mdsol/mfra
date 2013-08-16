1da5375ed19f4a586e82430cd4724dabb551eb94
rmeinesz-mdsol <rmeinesz@mdsol.com>
2013-08-16 10:25:05 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mfra::Application.config.secret_key_base = '80d02e722d72ee2057b692a71a4ff8f4685394a32b9efc90dba9367958563c6a1a7fdb8f32693bc54ad94de812f2f4eaff05f640d7f604f3324c3dbb3f796ba1'
